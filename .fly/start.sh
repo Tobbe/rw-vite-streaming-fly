377162f02c42d05b03b8c7577b272870e4c0a1d7
Tobbe Lundberg <tobbe@tlundberg.com>
2023-05-06 11:02:43 +0200

    Run fe-server in .fly/start

--- .fly/start.sh
+++ .fly/start.sh
@@ -6,4 +6,6 @@ if [ -n $MIGRATE_ON_BOOT ]; then
   $(dirname $0)/migrate.sh
 fi
 
-npx rw-server --port ${PORT} $@
+# npx rw-server --port ${PORT} $@
+node node_modules/@redwoodjs/api-server/dist/index.js api --port ${PORT} &
+yarn rw-serve-fe
